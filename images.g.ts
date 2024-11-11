e4e62cec81cc2e6d33432028d03465d5c1d5d756
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-11-11 17:05:01 +0100

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "konohalogo":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+1 1 1 1 1 1 f f c f f f f 1 f 1 
+1 1 1 1 c f d 1 1 1 1 1 f f 1 1 
+1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 f 1 1 1 c f f f 1 1 1 1 1 
+1 1 1 f 1 1 f f 1 1 1 f 1 1 1 1 
+1 1 f 1 1 1 f 1 1 1 1 1 f 1 1 1 
+1 1 f 1 1 f 1 1 1 1 1 1 f 1 1 1 
+1 1 f 1 1 f 1 1 1 1 c 1 f 1 1 1 
+1 c f 1 1 f 1 1 1 1 c 1 f 1 1 1 
+1 f c 1 1 f 1 1 1 c 1 1 c 1 1 1 
+1 c c 1 1 1 f 1 1 c 1 1 f 1 1 1 
+f 1 1 c 1 1 c f f 1 1 1 f 1 1 1 
+f 1 1 f d 1 1 1 1 1 1 f 1 1 1 1 
+c 1 1 1 f c 1 1 1 1 f c 1 1 1 1 
+f f f f f f f f f f 1 1 1 1 1 1 
+`;
+            case "image2":
+            case "sonidologo":return img`
+1 1 1 1 1 f d 1 1 1 1 1 1 1 1 1 
+1 1 1 1 d f f f f 1 1 1 1 1 1 1 
+1 1 1 1 d f 1 1 f f 1 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 f f 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 f 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 1 f 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 1 1 b f f d 
+1 1 1 1 d f 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 d f 1 1 1 1 1 1 1 1 1 1 
+1 1 1 f f f 1 1 1 1 1 1 1 1 1 1 
+1 f f f f f 1 1 1 1 1 1 1 1 1 1 
+c f f f f f 1 1 1 1 1 1 1 1 1 1 
+f f f f f 1 1 1 1 1 1 1 1 1 1 1 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
